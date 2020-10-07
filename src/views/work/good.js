ee88e9e7cbf6a9681a23b6b2fdd426571ecab253
YuanChengLang <709364178@qq.com>
2020-10-07 18:13:53 +0800

    fix

--- src/views/work/good.js
+++ src/views/work/good.js
@@ -2,6 +2,7 @@ import React,{useEffect,useState} from 'react';
 import {withRouter} from "react-router-dom";
 import {connect} from "react-redux";
 import {getGood,setGood,cancelGood} from "../../store/action/good";
+
 function Good(props){
     let {goodNub,good,goodid,user,dispatch,id,history} = props;
     let point = {};
